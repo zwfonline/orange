7bcf8c96f75a6a1005815004535d7252ebd8c080
Joe <146182656+zwfonline@users.noreply.github.com>
2024-12-23 14:05:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const images = [
+        "https://i.postimg.cc/gjjD1f32/webwxgetmsgimg-13.jpg",
+        "https://i.postimg.cc/wv6FQtvJ/webwxgetmsgimg-11.jpg",
+        "https://i.postimg.cc/k5NfSNnq/webwxgetmsgimg-10.jpg",
+        "https://i.postimg.cc/brwCpGf8/webwxgetmsgimg-5.jpg",
+    ];
+
+    let index = 0;
+    const hero = document.querySelector(".hero");
+
+    // 设置初始背景图片
+    hero.style.backgroundImage = `url(${images[index]})`;
+
+    function changeBackground() {
+        index = (index + 1) % images.length; // 循环切换图片
+        hero.style.backgroundImage = `url(${images[index]})`;
+    }
+
+    // 每 4 秒切换图片
+    setInterval(changeBackground, 4000);
+});
+
+
